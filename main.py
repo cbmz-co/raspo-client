5374e698ea08be3958712c5eb07c2867b7f861f3
mirko-z <mirko.zizzari@gmail.com>
2018-03-13 17:25:58 +0100

    primo prototipo

--- main.py
+++ main.py
@@ -2,19 +2,30 @@
 import asyncio
 import websockets
 import pyspeedtest
+import json
 from uuid import getnode as get_mac
+
+# TODO rewrite for python >3.4
+
+MAC_ADDRESS = str(get_mac())
+USR_TOKEN = input('Type your token: ')
 
 
 async def send_speed_test(ws):
     st = pyspeedtest.SpeedTest()
-    speed = pyspeedtest.pretty_speed(st.download())
-    await ws.send("MAC Address:"+mac+" Download Speed: "+speed)
+    ping = st.ping()
+    download_speed = pyspeedtest.pretty_speed(st.download())
+    upload_speed = pyspeedtest.pretty_speed(st.upload())
+    msg = {'token': USR_TOKEN, 'mac': MAC_ADDRESS, 'ping': ping, 'dw_speed': download_speed, 'up_speed': upload_speed}
+    json_msg = json.dumps(msg)
+    await ws.send(json_msg)
 
 
 async def open_ws(uri):
     async with websockets.connect(uri) as ws:
-        mac = str(get_mac())
-        await ws.send("MAC Address:" + mac)
+        msg = {'token': USR_TOKEN, 'mac': MAC_ADDRESS}
+        json_msg = json.dumps(msg)
+        await ws.send(json_msg)
         while True:
             try:
                 msg = await asyncio.wait_for(ws.recv(), timeout=60)
@@ -32,11 +43,13 @@ async def open_ws(uri):
                     ws.close()
                     return
             else:
+                # TODO Switch messages
                 print("Received a message: {" + msg + "}")
                 try:
                     await asyncio.wait_for(send_speed_test(ws), timeout=30)
                 except (asyncio.TimeoutError, websockets.ConnectionClosed) as error:
                     print(error)
+                    print("There are network problems... soon i'm trying to reconnect to the web socket.")
                     return
 
 
@@ -45,8 +58,13 @@ async def main():
         try:
             await open_ws('wss://ywxygb7-raspobackend.wedeploy.io/ciao')
             await asyncio.sleep(10)
-        except:
-            print("Something went wrong... retry connection.")
+        except (asyncio.TimeoutError, websockets.ConnectionClosed) as error:
+            print(error)
+            print("There are network problems... soon i'm trying to reconnect to the web socket.")
+            await asyncio.sleep(60)
+        except Exception as e:
+            print(e)
+            print("Something went wrong... retry.")
             await asyncio.sleep(60)
 
 
