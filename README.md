e5f269015f60910ac06e3b44971d106b046af213
mirko-z <mirko.zizzari@gmail.com>
2018-03-22 22:00:34 +0100

    gestione e invio messaggi migliorati

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # raspo-client
+
+pip install -r requirements.txt
+chmod +x main.py
+./main.py
--- main.py
+++ main.py
@@ -6,24 +6,44 @@ import json
 from uuid import getnode as get_mac
 
 MAC_ADDRESS = str(get_mac())
-USR_TOKEN = input('Type your token: ')
+USER_ID = "5ab0e9d8491df1ca3c49adf1"
+WS_URI = 'ws://localhost:8080/websocket/raspo?'+MAC_ADDRESS
 
 
 @asyncio.coroutine
 def send_speed_test(ws):
-    st = pyspeedtest.SpeedTest()
-    ping = round(st.ping(), 2)
-    download_speed = pyspeedtest.pretty_speed(st.download())
-    upload_speed = pyspeedtest.pretty_speed(st.upload())
-    msg = {'token': USR_TOKEN, 'mac': MAC_ADDRESS, 'ping': ping, 'dw_speed': download_speed, 'up_speed': upload_speed}
+    print("Preparing message for server...")
+    try:
+        st = pyspeedtest.SpeedTest()
+        st.connect('speedtestba1.telecomitalia.it')
+        ping = round(st.ping(), 2)
+        download_speed = pyspeedtest.pretty_speed(st.download())
+        upload_speed = pyspeedtest.pretty_speed(st.upload())
+        msg = {
+            'type': "speedTestClient",
+            'message': "",
+            'userId': USER_ID,
+            'mac': MAC_ADDRESS,
+            'ping': ping,
+            'dw_speed': download_speed,
+            'up_speed': upload_speed
+        }
+    except Exception as e:
+        print(e)
     json_msg = json.dumps(msg)
     yield from ws.send(json_msg)
+    print("Message sent to the web socket: " + json_msg)
 
 
 @asyncio.coroutine
 def open_ws(uri):
     ws = yield from websockets.connect(uri)
-    msg = {'token': USR_TOKEN, 'mac': MAC_ADDRESS}
+    msg = {
+        'type': "initClient",
+        'message': "",
+        'userId': USER_ID,
+        'mac': MAC_ADDRESS
+    }
     json_msg = json.dumps(msg)
     yield from ws.send(json_msg)
     while True:
@@ -36,6 +56,7 @@ def open_ws(uri):
         except asyncio.TimeoutError:
             # No data in 60 seconds, check the connection.
             try:
+                print("Received a timeout, sending a ping... ")
                 pong_waiter = yield from ws.ping()
                 yield from asyncio.wait_for(pong_waiter, timeout=10)
             except asyncio.TimeoutError:
@@ -43,22 +64,35 @@ def open_ws(uri):
                 print("No response from server, i try to reconnect!")
                 ws.close()
                 return
+            except Exception as e:
+                print("Unhandled exception on ping: " + e)
+        except Exception as e:
+            print("Unhandled exception on recv(): " + e)
         else:
             # TODO Switch messages
-            print("Received a message: " + msg )
             try:
-                yield from asyncio.wait_for(send_speed_test(ws), timeout=30)
-            except (asyncio.TimeoutError, websockets.ConnectionClosed) as error:
-                print(error)
-                print("There are network problems... soon i'm trying to reconnect to the web socket.")
-                return
+                json_msg = json.loads(msg)
+            except json.JSONDecodeError:
+                print("Received a NON-JSON Document : " + msg)
+            except Exception as e:
+                print("Unhandled exception on parse json: " + e)
+            else:
+                print("Received a JSON Document: " + json.dumps(json_msg))
+                try:
+                    yield from asyncio.wait_for(send_speed_test(ws), timeout=30)
+                except (asyncio.TimeoutError, websockets.ConnectionClosed) as error:
+                    print(error)
+                    print("There are network problems... soon i'm trying to reconnect to the web socket.")
+                    return
+                except Exception as e:
+                    print("Unhandled exception on send message: " + e)
 
 
 @asyncio.coroutine
 def main():
     while True:
         try:
-            yield from open_ws('wss://ywxygb7-raspobackend.wedeploy.io/ciao')
+            yield from open_ws(WS_URI)
             yield from asyncio.sleep(10)
         except (asyncio.TimeoutError, websockets.ConnectionClosed) as error:
             print(error)
